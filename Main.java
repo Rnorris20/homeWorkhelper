382b8575a35f45f7983725a6dfb4239d39de5737
RNorris6 <ryan.norris@patelhs.org>
2020-10-14 12:50:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.lang.model.util.ElementScanner6;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    boolean homeWork = true;
+
+    if (homeWork)
+    {
+      System.out.println("I have completed my homework!"");
+    }
+    else
+    {
+      System.out.println("My Homework is not yet done!"");
+    }
+  }
+}
