3a4e71cfcb6fc8012f3580c153922eb009ca84ab
RNorris6 <ryan.norris@patelhs.org>
2020-10-14 12:51:52 +0000

    I added a if/ else statement to check whether or not i completed my homework.

--- Main.java
+++ Main.java
@@ -2,16 +2,16 @@ import javax.lang.model.util.ElementScanner6;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
     boolean homeWork = true;
 
     if (homeWork)
     {
-      System.out.println("I have completed my homework!"");
+      System.out.println("I have completed my homework!");
     }
     else
     {
-      System.out.println("My Homework is not yet done!"");
+      System.out.println("My Homework is not yet done!");
     }
   }
 }
