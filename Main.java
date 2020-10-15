5584cd81a5d534a0de1739ce6748d03a177bfbe1
RNorris6 <ryan.norris@patelhs.org>
2020-10-15 23:46:46 +0000

    fix

--- Main.java
+++ Main.java
@@ -1,19 +1,22 @@
 import javax.lang.model.util.ElementScanner6;
 
+import java.until.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    
     boolean homeWork = true;
-    boolean problemOne = true;
-    boolean problemTwo = true;
-    Snanner Scan = new Scanner ();
-    if (homeWork && problemOne && problemTwo)
+
+    Scanner Scan = new Scanner (System.in);
+    System.out.println("What was your answer for Problem 1?");
+    int problemOne = scan.nextInt();
+
+    if (problemOne >= 10);
     {
       System.out.println("I have completed my homework!");
     }
-    else
+   else (problemOne < 10);
     {
-      System.out.println("My Homework is not yet done!");
+      System.out.println("This answer is wrong, Keep working!");
     }
   }
 }
