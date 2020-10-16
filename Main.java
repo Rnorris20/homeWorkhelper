4b9e380116d30e38414f0b824ae8abe7b713a3ed
RNorris6 <ryan.norris@patelhs.org>
2020-10-16 00:06:53 +0000

    I just revised my if and else statement as well as declaring a new object.

--- Main.java
+++ Main.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    boolean homeWork = true;
     boolean lawnMowed = true;
 
-    Scanner Scan = new Scanner (System.in);
-    System.out.println("What was your answer for Problem 1?");
-    int problemOne = Scan.nextInt();
+    Scanner scan = new Scanner (System.in);
+    System.out.println("Is your homework done, 'true' or 'false'?");
+    boolean homeWork = scan.nextBoolean();
 
-    if (problemOne >= 10 && lawnMowed) {
-      System.out.println("Nice job, you got this problem correct, now you can play at your friends house");
-    } else if (problemOne > 7) {
-      System.out.println("The answer is wrong, you cannot go to your friends house ");
+    if ( homeWork == true && lawnMowed) {
+      System.out.println("Nice job, now you can play at your friends house");
+
+    } else if ( homeWork == false) {
+
+      System.out.println("Sorry but you can't go to your friends house.");
     }
   }
 }
