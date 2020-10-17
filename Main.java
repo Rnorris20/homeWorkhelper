4b8fe7e1ca31b078a497832a604f65b4debe96c9
RNorris6 <ryan.norris@patelhs.org>
2020-10-17 01:57:23 +0000

    I put in a loop into my code

--- Main.java
+++ Main.java
@@ -9,11 +9,21 @@ class Main {
     boolean homeWork = scan.nextBoolean();
 //Asking the audience to see if you they completed their homework.
     if ( homeWork == true && lawnMowed) {
-      System.out.println("Nice job, now you can play at your friends house");
+      System.out.println("Nice job, now you can move onto the next problem.");
+      
+      System.out.println("You still have to complete the next couple of problems to go to your friends house.");
+
+     for(int count = 2; count <= 10; count++)
+     
+        System.out.println(count);
+
 
     } else if ( homeWork == false) {
 
-      System.out.println("Sorry but you can't go to your friends house today.");
+
+      System.out.println("Once you complete these problems you are done with your homework!");
+
+      }
+     }
     }
-  }
-}
+  
